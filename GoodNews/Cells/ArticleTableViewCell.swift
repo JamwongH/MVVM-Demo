c2ca42276f8d944fb42b47ca18d25f1ef4cb2aac
JamwongH <1014843431@qq.com>
2021-10-29 16:09:39 +0800

    Creating Custom Self Sizing Cells

---
+++ GoodNews/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ArticleTableViewCell.swift
+//  GoodNews
+//
+//  Created by Jamwong on 2021/10/29.
+//  Copyright © 2021 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class ArticleTableViewCell: UITableViewCell{
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
